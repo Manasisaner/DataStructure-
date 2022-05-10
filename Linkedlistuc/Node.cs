8780ff564da00927bd1ab5fb8e15194c3f066f25
snehasonwane <snehasonwane01@gmail.com>
2022-05-10 21:02:41 +0530

    [Manasi] Add: Create linkedlist

---
+++ Linkedlistuc/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlistuc
+{
+    internal class Node
+    {
+        public int data; // i have define two element overther
+        public Node next;
+
+        //constructor
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
+
--- Linkedlistuc/Program.cs
+++ Linkedlistuc/Program.cs
@@ -6,14 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataStructure
+namespace Linkedlistuc
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("-------Welcome To Data Structure Program-------");
+            LinkedList list = new LinkedList();//creating object of linkedlist class
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
             Console.Read();
+          
         }
     }
 }
